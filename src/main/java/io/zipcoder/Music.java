eb2e6c2bfeef761492daa3cbcacb0dd7e8c04a3e
Deepti Mishra <dmishra@Zipcoders-MBP.fios-router.home>
2024-11-06 22:44:56 -0500

    commit2

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -12,28 +12,28 @@ public class Music {
 
     public Integer selection(Integer startIndex, String selection) {
 
-        int count =0;
+        int count = 0;
 
-        int i =0;
-        while(selection!= playList[i]) {
-            if (startIndex < playList.length - 1 && startIndex > 0 ) {
+        if (startIndex > playList.length) {
+            System.out.println("End of the song list");
+        }
+
+        if (startIndex == 0 && selection.equals(playList[playList.length - 1])) {
+            System.out.println("Value at end of array: " + playList[playList.length - 1]);
+            System.out.println("start index : " + startIndex);
+            count++;
+           // startIndex = i;
+            return count;
+        }
+
+        int i = 0;
+        while (selection != playList[i]) {
+            if (startIndex < playList.length - 1 && startIndex > 0) {
                 startIndex++;
                 count++;
             }
             i++;
         }
-
-            if (startIndex == 0 && selection == playList[playList.length - i]) {
-                System.out.println("Value at end of array: " + playList[playList.length - 1]);
-                System.out.println("start index : " + startIndex);
-                count++;
-                startIndex = i;
-            }
-
-            if(startIndex > playList.length){
-                System.out.println("End of the song list");
-            }
-
 
             return count;
     }
